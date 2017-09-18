d4f4361cadc2352608f5d95b2681959db003735f
DK\anr <anr@sitecore.net>
2017-09-18 17:07:32 +0300

    introduced concept of EcmTest

---
+++ src/Sitecore.DiagnosticsTool.Tests/ECM/EcmTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.DiagnosticsTool.Core.Categories;
+using Sitecore.DiagnosticsTool.Core.Tests;
+using Sitecore.DiagnosticsTool.Tests.ECM.Helpers;
+
+namespace Sitecore.DiagnosticsTool.Tests.ECM
+{
+  public abstract class EcmTest : Test
+  {
+    public override IEnumerable<Category> Categories { get; } = new[] { Category.Ecm };
+    public virtual bool IsEcmVersionActual(EcmVersion ecmVersion)
+    {
+      return true;
+    }
+    public override void Process(ITestResourceContext data, ITestOutputContext output)
+    {
+      if (IsEcmVersionActual(EcmHelper.GetEcmVersion(data)))
+      {
+        DoProcess(data, output);
+      }
+      else
+      {
+        output.Debug("The test is not actual for current EXM version");
+      }
+    }
+
+    public abstract void DoProcess(ITestResourceContext data, ITestOutputContext output);
+
+  }
+}
